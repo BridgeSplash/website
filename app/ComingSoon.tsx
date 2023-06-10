fe0127355f043f7fb50c896412bc585dd7dc7826
TropicalShadow <ddlogger123@gmail.com>
2023-06-10 02:55:44 +0100

    Coming Soon Fixed formatting Fixed Background image Optimised title.svg

--- app/ComingSoon.tsx
+++ app/ComingSoon.tsx
@@ -1,14 +1,6 @@
-'use client';
-
-import { useMediaQuery } from 'react-responsive'
-
-
 export default function ComingSoon() {
-    const isDesktopOrLaptop = useMediaQuery({
-        query: '(min-width: 1224px)'
-      })
     return (
-        <div className={`z-5 fixed ${!isDesktopOrLaptop ? 'bottom-0' : 'top-0'} left-0 w-full bg-blue-500 text-white py-2 px-4`}>
+        <div className={`z-5 fixed bottom-0 lg:bottom-auto lg:top-0 left-0 w-full bg-blue-500 text-white py-2 px-4`}>
         <p className="text-center">
           BridgeSplash Minecraft Network is coming soon!<br />
           Early access to SMP & Oneblock is available now.
