d026ef049079aaffdc29c122e6faaa81d407e31a
TropicalShadow <52831503+TropicalShadow@users.noreply.github.com>
2023-06-17 03:50:24 +0100

    Add redirects - Donation - Store

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
-    
-}
+  output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/donate",
+        destination: "https://donate.stripe.com/7sI14a13paAb7TO9AA",
+        permanent: true,
+      },
+      {
+        source: "/store",
+        destination: "https://store.bridgesplash.net",
+        permanent: true,
+      }
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
